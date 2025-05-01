f148b64b2f40120e68557bee0128161552f14c90
pr0gresss <tsublenokd@mail.ru>
2025-05-01 11:15:42 +0300

    fix: Code style fixes;

--- src/app/shared/services/ProductService.ts
+++ src/app/shared/services/ProductService.ts
@@ -1,17 +1,19 @@
-import { IProduct } from "@interfaces";
+import { IProduct } from '@interfaces';
 
-export const getProducts  = async (): Promise<IProduct[]> => {
-  try {
-    const response = await fetch("https://65de35f3dccfcd562f5691bb.mockapi.io/api/v1/meals");
+export const getProducts = async (): Promise<IProduct[]> => {
+	try {
+		const response = await fetch(
+			'https://65de35f3dccfcd562f5691bb.mockapi.io/api/v1/meals'
+		);
 
-    if (!response.ok) {
-      throw new Error("Can't establish connection with API.");
-    }
+		if (!response.ok) {
+			throw new Error("Can't establish connection with API.");
+		}
 
-    const data: IProduct[] = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Error fetching product data:", error);
-    throw error;
-  }
-}
+		const data: IProduct[] = await response.json();
+		return data;
+	} catch (error) {
+		console.error('Error fetching product data:', error);
+		throw error;
+	}
+};
